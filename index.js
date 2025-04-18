f09fe50ea08aaca0d42dd3822bc0a982ddfbcd35
Khizar <khizar13ahmed@gmail.com>
2025-04-18 17:57:16 +0500

    origins

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT;
 const app = express();
 const cors = require("cors");
 
-const allowedOrigins = ["http://localhost:3000", "https://crm.paylinkly.com"];
+const allowedOrigins = ["http://localhost:3000", "https://nucleus.sale"];
 
 app.use(
   cors({
