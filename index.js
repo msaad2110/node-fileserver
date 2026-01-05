b9c134e720a280e1b92944b9e65605b22b23ea21
muhammdfayyazansari <fayyazansari268@gmail.com>
2026-01-06 04:21:37 +0500

    update the changes

--- index.js
+++ index.js
@@ -39,9 +39,7 @@ app.use(securityLogger);
 
 app.use("/api", file);
 app.use("/public", express.static(path.join(__dirname, "public")));
-app.use("/testing-purposes", (req, res) => {
-  res.send("Testing purposes only");
-});
+
 // Error logging middleware (must be after routes)
 app.use(errorLogger);
 
